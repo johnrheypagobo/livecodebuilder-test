9ae84345d07a30e7b42d94b2bd1c61d9b62626b5
jpags1024 <41568125+jpags1024@users.noreply.github.com>
2019-03-19 11:58:08 +0800

    this is the code for the dll file

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_dll
+{
+    public class test_Multiply  
+    {
+        public int multiply_numbers(int x, int y)
+        {
+            int num1 = x;
+            int num2 = y;
+
+            return num1 * num2;
+        }
+    }
+}
